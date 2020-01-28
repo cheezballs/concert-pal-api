1097f8fb4d6c4383ce3c988e425b7324c6340871
Matt Roberts <sweetcrapmatt@gmail.com>
2020-01-27 20:18:00 -0600

    Tests!

---
+++ Concert Pal Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Concert_Pal_Tests
+{
+   public class Tests
+   {
+      [SetUp]
+      public void Setup()
+      {
+      }
+
+      [Test]
+      public void Test1()
+      {
+         Assert.Pass();
+      }
+   }
+}
